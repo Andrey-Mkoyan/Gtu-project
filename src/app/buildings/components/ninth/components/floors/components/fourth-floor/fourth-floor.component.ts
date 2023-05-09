87a9eaa9ce1bdf028820c4aca6e5c92c2cd9aa11
Andrey_Mkoyan <and.mkoyan07@gmail.com>
2023-05-09 19:15:52 +0400

    commit

---
+++ src/app/buildings/components/ninth/components/floors/components/fourth-floor/fourth-floor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fourth-floor',
+  templateUrl: './fourth-floor.component.html',
+  styleUrls: ['./fourth-floor.component.scss']
+})
+export class FourthFloorComponent {
+
+}
---
+++ src/app/buildings/components/ninth/components/floors/components/second-floor/second-floor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-second-floor',
+  templateUrl: './second-floor.component.html',
+  styleUrls: ['./second-floor.component.scss']
+})
+export class SecondFloorComponent {
+
+}
