8c144200c9435e06f85cef45464c188cd52eb785
Andrey-Mkoyan <and.mkoyan07@gmail.com>
2023-05-28 22:36:32 +0400

    fixes

--- src/app/buildings/buildings.component.ts
+++ src/app/buildings/buildings.component.ts
@@ -10,56 +10,58 @@ export class BuildingsComponent {
   constructor() {
     this.cards = [
       {
-        title: 'First building',
+        title: 'პირველი კორპუსი',
         path: '/first-building',
         image: '../../assets/backgrounds/first-building.jpg',
         faculties: '1,2,3,4,5' ,
         id: 1,
       },
       {
-        title: 'Second building',
+        title: 'მეორე კორპუსი',
         path: 'second-building',
         image: '../../assets/backgrounds/2BuildingBackImage.jpg',
         faculties: '1,2,3,4,5',
         id:2
       },
       {
-        title: 'Third building',
+        title: 'ადმინისტრაცუილი კორპუსი',
         path: 'third-building',
         image: '../../assets/backgrounds/main.jpg',
+        faculties: '1,2,3,4,5',
+        id:10
+      },
+
+      {
+        title: 'მესამე კორპუსი',
+        path: '/fourth-building',
+        image: '../../assets/backgrounds/3BuildingBackImage.jpg',
         faculties: '1,2,3,4,5',
         id:3
       },
       {
-        title: 'Fourth building',
-        path: '/fourth-building',
-        image: '../../assets/backgrounds/3BuildingBackImage.jpg',
+        title: 'მეოთხე კორპუსი',
+        path: '/seventh-building',
+        image: '../../assets/backgrounds/4კორპუსი.jpeg' ,
         faculties: '1,2,3,4,5',
         id:4
       },
       {
-        title: 'Sixth building',
+        title: 'მეექვსე კორპუსი',
         path: '/sixth-building',
         image: '../../assets/backgrounds/6-კორპუსი.jpg' ,
         faculties: '1,2,3,4,5',
         id:6
       },
+     
       {
-        title: 'Seventh building',
-        path: '/seventh-building',
-        image: '../../assets/backgrounds/7-კორპუსი.jpg' ,
-        faculties: '1,2,3,4,5',
-        id:7
-      },
-      {
-        title: 'Eighth building',
+        title: 'მერვე კორპუსი',
         path: '/eighth-building',
         image: '../../assets/backgrounds/8-კორპუსი.jpg',
         faculties: '1,2,3,4,5',
         id:8 
       },
       {
-        title: 'Ninth building',
+        title: 'მეცხრე',
         path: '/ninth-building',
         image: '../../assets/backgrounds/9-კორპუსი.jpeg',
         faculties: '1,2,3,4,5' ,
